4deb15b81b6bfe94205d7979cd16472ae167d145
Yashmit420 <yashmitdhull01@gmail.com>
2026-02-24 06:21:24 +0530

    Add files via upload

---
+++ repurposer/repurposer/src/main/java/com/ai/repurposer/RepurposerApplication.java
@@ -0,0 +1,13 @@
+package com.ai.repurposer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepurposerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepurposerApplication.class, args);
+	}
+
+}
